dbe029f5015eb5dead8221bbcd46f706a2a26b82
Fredrik Stetler <Fredrik.Stetler@knightec.se>
2024-04-25 10:57:39 +0200

    saved

--- src/main/java/org/example/springPlayground/service/MovieService.java
+++ src/main/java/org/example/springPlayground/service/MovieService.java
@@ -4,7 +4,6 @@ import org.example.springPlayground.model.Movie;
 import org.example.springPlayground.repo.MovieRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
